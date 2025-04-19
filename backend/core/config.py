c647d824ed6359114989594b3d561e0bd9296ca2
Григорий Трусов <trusovgrisha@gmail.com>
2025-04-19 13:08:17 +0300

    commit

--- backend/core/config.py
+++
@@ -1,8 +0,0 @@
-class Settings:
-    DB_NAME = "Planer"
-    DB_USER = "postgres"
-    DB_PASSWORD = "2002"
-    DB_HOST = "localhost"
-    DB_PORT = "5432"
-
-settings = Settings()
--- backend/modules/prediction/timeline_prediction.py
+++
@@ -1,8 +0,0 @@
-from sklearn.linear_model import LinearRegression
-import numpy as np
-
-def predict_task_completion_time(features: np.array, target: np.array, new_task: np.array) -> float:
-    """Предсказывает время завершения задачи на основе исторических данных."""
-    model = LinearRegression()
-    model.fit(features, target)
-    return model.predict([new_task])[0]
